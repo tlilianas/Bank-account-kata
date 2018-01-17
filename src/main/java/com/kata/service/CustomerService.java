19567da968b851f0fec8f1c3e2d1a6e0c16db1eb
Anas Tlili <anas.tlili@gmail.com>
2018-01-17 12:18:42 +0100

    clean the room

--- src/main/java/com/kata/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.kata.service;
-
-import com.kata.model.Customer;
-
-public interface CustomerService {
-    public Customer getCustomerbyId(long id);
-    public Customer saveCustomer(Customer customer);
-    public void removeCustomer(Customer customer);
-}
--- src/main/java/com/kata/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.kata.util;
-
-public class Constants {
-    public static final String WITHDRAWAL = "Withdrawal ";
-    public static final String DEPOSIT = "Deposit ";
-}
